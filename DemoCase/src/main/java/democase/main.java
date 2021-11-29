4a30ccaa3a1d20086eae186c86e18770c81102f6
Duc Huy Nguyen <stdcnguy>
2021-11-29 20:22:18 +0100

    small fix

--- DemoCase/src/main/java/democase/main.java
+++ DemoCase/src/main/java/democase/main.java
@@ -6,10 +6,6 @@ import server.UDPSocketServer;
 public class main {
     public static void main(String[] args) {
 
-        Laden laden = new Laden();
 
-        System.out.println("Starting Laden Server");
-
-        laden.run();
     }
 }
