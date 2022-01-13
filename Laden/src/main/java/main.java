f84b5fae7ddb219959bb14be94643daa08ba6c6d
Duc Huy Nguyen <stdcnguy>
2022-01-13 19:56:22 +0100

    tcp stress testing

--- Laden/src/main/java/main.java
+++ Laden/src/main/java/main.java
@@ -1,31 +1,34 @@
 import Laden.Laden;
 import Laden.LadenTest;
-import configuration.ServerCliParser;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.util.EntityUtils;
 import org.apache.thrift.transport.TTransportException;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class main {
-    public static void startSystem(String[] args) throws IOException, InterruptedException {    
+    public static void startSystem(String[] args) throws IOException, InterruptedException {
         //        ServerCliParser.parse(args);
         Laden laden = new Laden();
 
         System.out.println("Starting Laden Server with id " + laden.getId());
 
-        Thread udp = new Thread(){
-            public void run(){
+        Thread udp = new Thread() {
+            public void run() {
                 laden.runUDPServer();
             }
         };
 
-        Thread tcp = new Thread(){
-            public void run(){
+        Thread tcp = new Thread() {
+            public void run() {
                 laden.runTCPServer();
             }
         };
 
-        Thread rpc = new Thread(){
-            public void run(){
+        Thread rpc = new Thread() {
+            public void run() {
                 try {
                     laden.runRPCServer();
                 } catch (TTransportException e) {
@@ -38,9 +41,9 @@ public class main {
         tcp.start();
         rpc.start();
 
-        if (args == null || args.length == 0 || args[0] == null || args[0].equalsIgnoreCase("")){
+        if (args == null || args.length == 0 || args[0] == null || args[0].equalsIgnoreCase("")) {
 
-        }else{
+        } else {
             laden.simulateLadenExchanges(args[0]);
         }
 
@@ -49,15 +52,17 @@ public class main {
     public static void main(String[] args) throws IOException, InterruptedException {
 //        startSystem(args);
 //        runLadenUDPStressTest();
-        runLadenRPCStressTest(args);
+//        runLadenRPCStressTest(args);
+        runLadenTCPStressTest();
+
     }
 
 
     public static void runLadenUDPStressTest() throws IOException {
         LadenTest laden = new LadenTest();
         System.out.println("Starting Laden Server with id " + laden.getId());
-        Thread udp = new Thread(){
-            public void run(){
+        Thread udp = new Thread() {
+            public void run() {
                 laden.runUDPServer();
             }
         };
@@ -66,12 +71,12 @@ public class main {
 
     public static void runLadenRPCStressTest(String[] args) throws IOException, InterruptedException {
         LadenTest laden = new LadenTest();
-        if (args.length > 0){
+        if (args.length > 0) {
             laden.simulateLadenExchanges(args[0]);
-        }else{
+        } else {
             laden.populateInventory();
-            Thread rpc = new Thread(){
-                public void run(){
+            Thread rpc = new Thread() {
+                public void run() {
                     try {
                         laden.runRPCServer();
                     } catch (TTransportException e) {
@@ -82,5 +87,53 @@ public class main {
             rpc.start();
         }
     }
+
+    public static void runLadenTCPStressTest() throws IOException {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input port number of this docker container");
+        String port = sc.nextLine();
+        String url = "localhost:" + port;
+        final int totalAmountOfHttpRequest = 10000;
+        int amountOfSucceedRequest = 0;
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < totalAmountOfHttpRequest; i++){
+            if (makeHttpRequest(url)){
+                amountOfSucceedRequest++;
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("HTTP test took total of " + (end - start));
+        System.out.println("Amount of request lost: " + (amountOfSucceedRequest - totalAmountOfHttpRequest));
+        System.out.println("Average time taken for each request: " + ((double)totalAmountOfHttpRequest/(end - start)));
+    }
+
+    private static boolean makeHttpRequest(String url) throws IOException {
+        Request request = Request.Get(url);
+        HttpResponse httpResponse = request.execute().returnResponse();
+        System.out.println(httpResponse.getStatusLine());
+        if (httpResponse.getEntity() != null) {
+            String html = EntityUtils.toString(httpResponse.getEntity());
+            return html.split("\n")[0].contains("OK");
+        }
+        return false;
+    }
+
+//    public static void runLadenTCPStressTest(){
+//        //Input port
+
+//        StringBuilder url = new StringBuilder("localhost:" + port);
+//        String[] cmds = {"curl", url.toString()};
+//        ProcessBuilder processBuilder = new ProcessBuilder(cmds);
+//        Process p;
+//        try {
+//            p = processBuilder.start();
+//            BufferedReader br = null;
+//            String line = null;
+//            br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+//            while ((line = br.readLine()))
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
